2b0a8629e2a084eee19af9f724faeb0b61275697
Tristan Montoya <montoya.tristan@gmail.com>
2023-05-04 18:29:06 -0400

    update p-error array script

--- scripts/make_p_error_arrays.jl
+++ scripts/make_p_error_arrays.jl
@@ -1,9 +1,11 @@
 using CLOUD, JLD2
 
-paths = ["/project/z/zingg/tmontoya/TensorSimplexResults/20230429_p/NodalMulti_Tri_SkewSymmetricMapping/upwind/",
-     "/project/z/zingg/tmontoya/TensorSimplexResults/20230429_p/NodalMulti_Tri_SkewSymmetricMapping/central/",
-     "/project/z/zingg/tmontoya/TensorSimplexResults/20230429_p/ModalTensor_Tri_SkewSymmetricMapping/upwind/",
-      "/project/z/zingg/tmontoya/TensorSimplexResults/20230429_p/ModalTensor_Tri_SkewSymmetricMapping/central/"]
+
+
+paths = ["/project/z/zingg/tmontoya/TensorSimplexResults/20230429_p/NodalMulti_Tet_SkewSymmetricMapping/upwind/",
+     "/project/z/zingg/tmontoya/TensorSimplexResults/20230429_p/NodalMulti_Tet_SkewSymmetricMapping/central/",
+     "/project/z/zingg/tmontoya/TensorSimplexResults/20230429_p/ModalTensor_Tet_SkewSymmetricMapping/upwind/",
+      "/project/z/zingg/tmontoya/TensorSimplexResults/20230429_p/ModalTensor_Tet_SkewSymmetricMapping/central/"]
 
 p_min=2
 p_max=25
@@ -12,6 +14,9 @@ for path in paths
     save_object(string(path, "poly_degrees.jld2"), Int64[])
     save_object(string(path, "errors.jld2"), Float64[])
     for p in p_min:p_max
+        if !isfile(string(path, "p", p, "/", "error.jld2"))
+            continue
+        end
         errors=load_object(string(path, "errors.jld2"))
         error=load(string(path, "p", p, "/", "error.jld2"))["error"][1]
 
