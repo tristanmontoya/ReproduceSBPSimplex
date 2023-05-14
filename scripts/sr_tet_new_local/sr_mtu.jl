464cf04b148f05bab7c9e610341321a9557cdfb3
Tristan Montoya <montoya.tristan@gmail.com>
2023-05-14 13:36:01 -0400

    scripts for paper draft work

--- scripts/sr_tet_new_local/sr_mtu.jl
+++ scripts/sr_tet_new_local/sr_mtu.jl
@@ -1,4 +1,4 @@
 using TensorSimplexTests
 
-run_driver(AdvectionPRefinementDriver(9, 25, scheme="ModalTensor", M0=2,
+run_driver(AdvectionPRefinementDriver(13, 25, scheme="ModalTensor", M0=2,
     element_type="Tet", λ=1.0, l=2, mesh_perturb=1.0/16.0, run=false, spectral_radius=true, path="../results/sr_tet_local/", load_from_file=true))
--- scripts/sr_tet_new_local/sr_ntc.jl
+++ scripts/sr_tet_new_local/sr_ntc.jl
@@ -1,4 +1,4 @@
 using TensorSimplexTests
 
-run_driver(AdvectionPRefinementDriver(2, 10, scheme="NodalTensor", M0=2,
+run_driver(AdvectionPRefinementDriver(13, 25, scheme="NodalTensor", M0=2,
     element_type="Tet", λ=0.0, l=2, mesh_perturb=1.0/16.0, run=false, spectral_radius=true, path="../results/sr_tet_local/", load_from_file=true))
